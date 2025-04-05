ef167530789fe0c5c14ea32c95679a7b1f2cf4aa
TrongNamNguyen <ngntrongnam7780@gmail.com>
2025-04-05 23:46:04 +0700

    ngntrongnam

--- app/src/main/java/com/example/qunnbnhyn/QuanLyNhanVien.java
+++ app/src/main/java/com/example/qunnbnhyn/QuanLyNhanVien.java
@@ -1,6 +1,9 @@
 package com.example.qunnbnhyn;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class QuanLyNhanVien extends AppCompatActivity {
-
+    private Button btnAdd, btnEdit, btnDelete, btnTimeKeeping;
+    private ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quan_ly_nhan_vien);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnBack = (ImageButton) findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
+
+
     }
 }
