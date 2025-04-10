5b9b00d1915cd08249e92f69418e172269c5546b
ntkl <linhntknbk04@gmail.com>
2025-04-10 22:03:17 +0700

    update

--- app/src/main/java/com/example/qunnbnhyn/MainActivity.java
+++ app/src/main/java/com/example/qunnbnhyn/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qunnbnhyn;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,13 +8,20 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView supplies, customService, happyClient, discount;
     private String maNhanVien;
     private DatabaseReference database;
+    private DatabaseReference timeTrackingRef;
     private TextView txtName;
+    private FirebaseAuth myAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +41,23 @@ public class MainActivity extends AppCompatActivity {
         // Lấy maNhanVien từ Intent trước
         Intent intent = getIntent();
         maNhanVien = intent.getStringExtra("maNhanVien");
+        if (maNhanVien == null || maNhanVien.isEmpty()) {
+            Toast.makeText(this, "Không tìm thấy mã nhân viên. Vui lòng đăng nhập lại.", Toast.LENGTH_SHORT).show();
+            Intent loginIntent = new Intent(MainActivity.this, Login.class);
+            startActivity(loginIntent);
+            finish();
+            return;
+        }
 
         // Kết nối tới node Employees trên Firebase (sau khi maNhanVien đã được gán)
         database = FirebaseDatabase.getInstance().getReference("Employees").child(maNhanVien);
+
+        // Kết nối tới node Chấm công
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        timeTrackingRef = FirebaseDatabase.getInstance().getReference("Cham_cong").child(currentDate).child(maNhanVien);
+
+        // Khởi tạo FirebaseAuth
+        myAuth = FirebaseAuth.getInstance();
 
         // Ánh xạ các view
         txtName = findViewById(R.id.txtName);
@@ -46,9 +70,8 @@ public class MainActivity extends AppCompatActivity {
         customService = findViewById(R.id.fram2).findViewById(R.id.custom_service);
         happyClient = findViewById(R.id.fram2).findViewById(R.id.happy_client);
         discount = findViewById(R.id.fram3).findViewById(R.id.discount);
-        logout = findViewById(R.id.logout);
+        logout = findViewById(R.id.fram3).findViewById(R.id.logout);
         finger = findViewById(R.id.finger);
-
 
         // Gắn sự kiện onClick cho ImageView "Quản lý bàn ăn"
         diningTableImageView.setOnClickListener(new View.OnClickListener() {
@@ -59,26 +82,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Sự kiện khi nhấn nút logout
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LogoutActivity.class);
-                startActivity(intent);
+                // Hiển thị dialog xác nhận đăng xuất
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Xác nhận đăng xuất")
+                        .setMessage("Bạn có chắc chắn muốn đăng xuất không?")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Lấy thời gian hiện tại
+                                String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+
+                                // Cập nhật thời gian check-out vào node Chấm công
+                                timeTrackingRef.child("check-out").setValue(currentTime).addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        // Đăng xuất khỏi Firebase Authentication
+                                        myAuth.signOut();
+
+                                        // Hiển thị thông báo đăng xuất thành công
+                                        Toast.makeText(MainActivity.this, "Đăng xuất thành công!", Toast.LENGTH_SHORT).show();
+
+                                        // Chuyển về LoginActivity và xóa toàn bộ activity stack
+                                      /*  Intent intent = new Intent(MainActivity.this, Login.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        finish();*/
+                                        // Thoát ứng dụng
+                                        finishAffinity();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Lỗi lưu giờ checkout: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss(); // Đóng dialog, không làm gì
+                            }
+                        })
+                        .show();
             }
         });
 
+        // Sự kiện khi nhấn nút finger (check-in)
         finger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                // Lấy thông tin ca làm việc (shift) từ node Employees
+                database.child("shift").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+                        Map<String, String> cong = new HashMap<>();
+                        cong.put("check-in",currentTime);
+                        cong.put("check-out","null");
+                        cong.put("shift",snapshot.getValue(String.class));
+                        FirebaseDatabase.getInstance().getReference("Cham_cong").child(currentDate).child(maNhanVien).setValue(cong);
 
-                // Lưu thời gian check-in vào Firebase/
-                database.child("checkIn").setValue(currentTime).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        // Hiển thị thông báo "Xin cảm ơn"
-                        Toast.makeText(MainActivity.this, "Xin cảm ơn", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Lỗi chấm công: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
                     }
                 });
             }
@@ -109,3 +179,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+
--- app/src/main/java/com/example/qunnbnhyn/MenuNVActivity.java
+++ app/src/main/java/com/example/qunnbnhyn/MenuNVActivity.java
@@ -1,21 +1,31 @@
 package com.example.qunnbnhyn;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MenuNVActivity extends AppCompatActivity {
 
@@ -25,66 +35,114 @@ public class MenuNVActivity extends AppCompatActivity {
     private DatabaseReference database;
     private TextView txtName;
 
+    private DatabaseReference timeTrackingRef;
+    private FirebaseAuth myAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Lấy maNhanVien từ Intent trước
-        Intent intent = getIntent();
-        maNhanVien = intent.getStringExtra("maNhanVien");
-
-        // Kết nối tới node Employees trên Firebase
+        // Kết nối tới node Employees trên Firebase (sau khi maNhanVien đã được gán)
         database = FirebaseDatabase.getInstance().getReference("Employees").child(maNhanVien);
 
+        // Kết nối tới node Chấm công
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        timeTrackingRef = FirebaseDatabase.getInstance().getReference("Cham_cong").child(currentDate).child(maNhanVien);
+
+        // Khởi tạo FirebaseAuth
+        myAuth = FirebaseAuth.getInstance();
+
         // Ánh xạ các view
+        Intent intent = getIntent();
         txtName = findViewById(R.id.txtName);
-        String fullName = intent.getStringExtra("full_name");
-        Log.d("name", fullName != null ? fullName : "null");
-        txtName.setText(fullName != null ? fullName : "Khách");
+        Log.d("name",intent.getStringExtra("full_name"));
+        txtName.setText(intent.getStringExtra("full_name"));
 
         // Ánh xạ ImageView "Quản lý bàn ăn" từ GridLayout
         diningTableImageView = findViewById(R.id.fram1).findViewById(R.id.dining_table_image);
         order = findViewById(R.id.fram1).findViewById(R.id.order);
         happyClient = findViewById(R.id.fram2).findViewById(R.id.happy_client);
         payment = findViewById(R.id.fram2).findViewById(R.id.payment);
-        logout = findViewById(R.id.logout);
+        logout = findViewById(R.id.fram3).findViewById(R.id.logout);
         finger = findViewById(R.id.finger);
 
 
-/*        finger.setOnClickListener(new View.OnClickListener() {
+        finger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                // Lấy thông tin ca làm việc (shift) từ node Employees
+                database.child("shift").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+                        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+                        Map<String, String> cong = new HashMap<>();
+                        cong.put("check-in",currentTime);
+                        cong.put("check-out","null");
+                        cong.put("shift",snapshot.getValue(String.class));
+                        FirebaseDatabase.getInstance().getReference("Cham_cong").child(currentDate).child(maNhanVien).setValue(cong);
 
-                // Lưu thời gian check-in vào Firebase/
-                database.child("checkIn").setValue(currentTime).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        // Hiển thị thông báo "Xin cảm ơn"
-                        Toast.makeText(MenuNVActivity.this, "Xin cảm ơn", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MenuNVActivity.this, "Lỗi chấm công: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                     }
-               });
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
-        });*/
+        });
 
         // Gắn sự kiện onClick cho ImageView "Quản lý bàn ăn"
         diningTableImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Chuyển sang ManageTableActivity
-                   Intent intent = new Intent(MenuNVActivity.this, NVListActivity.class);
-                    startActivity(intent);
+                Intent intent = new Intent(MenuNVActivity.this, NVListActivity.class);
+                startActivity(intent);
             }
         });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Chuyển sang LogoutActivity
-                Intent intent = new Intent(MenuNVActivity.this, LogoutActivity.class);
-                startActivity(intent);
+                // Hiển thị dialog xác nhận đăng xuất
+                new AlertDialog.Builder(MenuNVActivity.this)
+                        .setTitle("Xác nhận đăng xuất")
+                        .setMessage("Bạn có chắc chắn muốn đăng xuất không?")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Lấy thời gian hiện tại
+                                String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+
+                                // Cập nhật thời gian check-out vào node Chấm công
+                                timeTrackingRef.child("check-out").setValue(currentTime).addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        // Đăng xuất khỏi Firebase Authentication
+                                        myAuth.signOut();
+
+                                        // Hiển thị thông báo đăng xuất thành công
+                                        Toast.makeText(MenuNVActivity.this, "Đăng xuất thành công!", Toast.LENGTH_SHORT).show();
+
+                                        // Chuyển về LoginActivity và xóa toàn bộ activity stack
+                                        Intent intent = new Intent(MenuNVActivity.this, Login.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(MenuNVActivity.this, "Lỗi lưu giờ checkout: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss(); // Đóng dialog, không làm gì
+                            }
+                        })
+                        .show();
             }
         });
 
