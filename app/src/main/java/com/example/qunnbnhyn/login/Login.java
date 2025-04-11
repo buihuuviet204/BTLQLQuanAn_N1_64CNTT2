41144b219c6b1216c293a42740c93f5cdae8a8c5
nguye <nguyenhuynh02032004@gmail.com>
2025-04-11 11:12:34 +0700

    xoa main

--- app/src/main/java/com/example/qunnbnhyn/login/Login.java
+++ app/src/main/java/com/example/qunnbnhyn/login/Login.java
@@ -8,10 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.qunnbnhyn.ActivityMainNV;
-import com.example.qunnbnhyn.MainActivity;
 import com.example.qunnbnhyn.QLM.MonAn;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,17 +20,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.qunnbnhyn.R;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseUserMetadata;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Login extends AppCompatActivity {
     private Button btn_login;
